22543ae0c510fecf29586e64382813af2024632e
Rachel Sanham <green_chul@hotmail.com>
2021-10-10 17:13:15 +0100

    Booleans WIP

--- src/booleans.js
+++ src/booleans.js
@@ -1,58 +1,114 @@
 function negate(a) {
-  // your code here
+  return !a
 };
 
 function both(a, b) {
-  // your code here
+  if (a && b){
+    return true
+  }
+  else{
+    return false
+  }
 };
 
 function either(a, b) {
-  // your code here
+  if (a || b){
+    return true
+  }
+  else{
+    return false
+  }
 };
 
 function none(a, b) {
-  // your code here
+ if (!a && !b){
+   return true
+ }
+ else {
+   return false
+ }
 };
 
 function one(a, b) {
-  // your code here
+  if ((a && !b)||(!a && b)){
+    return true
+  }
+  else{
+    return false
+  }
 };
 
 function truthiness(a) {
-  // your code here
+  if (a){
+    return true
+  }
+  else{
+    return false
+  }
 };
 
 function isEqual(a, b) {
-  // your code here
+  // if (a === b){
+  //   return true
+  // }
+  // else{
+  //   return false
+  // }
+  return a === b ? true : false
 };
 
 function isGreaterThan(a, b) {
-  // your code here
+  return a > b ? true : false
 };
 
 function isLessThanOrEqualTo(a, b) {
-  // your code here
+  return a <=b ? true : false
 };
 
 function isOdd(a) {
-  // your code here
+  // if(a%2 > 0){
+  //   return true
+  // }
+  // else{
+  //   return false
+  // }
+  return (a%2 >0) ? true : false
 };
 
 function isEven(a) {
-  // your code here
+  return (a%2 === 0) ? true : false
 };
 
 function isSquare(a) {
-  // your code here
+  let squareRoot = Math.sqrt(a)
+  return (squareRoot**2 === a) ? true : false 
 };
 
 function startsWith(char, string) {
-  // your code here
+  if (string[0] === char){
+    return true
+  }
+  else{
+    return false
+  }
 };
 
 function containsVowels(string) {
-  // your code here
-};
+  let vowels = ["a", "e", "i", "o", "u"]
+  string = string.toLowerCase();
+  // loop through each vowel and check if it is in the string
+  // if so return true
+  // once every letter has been checked return false if no vowel
+  for(let i = 0; i< vowels.length; i++){
+    if(string.indexOf(vowels[i])>=0){
+      return true
+    }
+   
+    }
+    return false
+  }
+  
+
 
 function isLowerCase(string) {
   // your code here
